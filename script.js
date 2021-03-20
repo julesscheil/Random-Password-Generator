4c8553ea4326a2591d30347a0979b0db102fdc3d
Jules Scheil <juliana.m.scheil@gmail.com>
2021-03-20 16:48:45 -0400

    Added password length function prompts.

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passLength;
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var numericChar = "0123456789";
+var specialChar = "!@#$%^&*()_-+={}[];:'`~<,>.?/|"
+
+// Password length prompt function
+function passwordLengthPrompt(){
+  passwordLength = prompt("How many characters would you like your password to be? (8-128 characters)");
+  if (passwordLength<8) {
+    alert("Password length too short, must be between 8 - 128 characters. Try again.");
+    passwordLengthPrompt();
+  } else if (passwordLength>128) {
+    alert("Password length too long, must be between 8-128 characters. Try again.")
+    passwordLengthPrompt()
+  }else if (isNaN(passwordLength)) {
+    alert("Password length must be a number. Try again.");
+    passwordLengthPrompt();
+  }else {
+  }
+  return passwordLength;
+}
+passwordLengthPrompt();
 
 // Write password to the #password input
 function writePassword() {
@@ -8,6 +31,15 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+// Generate password function
+function generatePassword() {
+  
+  //code to ask user for parameters
+  //use password to fill parameters
+  //return password
+  return "Temporary Password";
 }
 
 // Add event listener to generate button
